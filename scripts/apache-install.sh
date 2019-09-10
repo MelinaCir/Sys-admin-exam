769d54e3a67b8cd5fcab23437c43ed4933013adf
MelinaCir <mc222ap@student.lnu.se>
2019-09-10 12:16:19 +0000

    Adding apache scripts

---
+++ scripts/apache-install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# This script installs Apache2 for Ubuntu18.04 and then creates directories and files needed for configuration
+
+source apache-functions.sh
+
+USERNAME='mc222ap'
+TESTUSER='test.mc222ap'
+
+header "Apache install and configure script"
+
+sudo apt-get update
+
+install_apache
+
+sudo systemctl start apache2
+
+# Creating directories for demo pages for the two sites. In this directories a index.html page should be created
+
+sudo mkdir -p /var/www/$USERNAME.devopslab.xyz/public_html
+sudo mkdir -p /var/www/$TESTUSER.devopslab.xyz/public_html
+sudo chown -R $ubuntu:$ubuntu /var/www/$USERNAME.devopslab.xyz/public_html
+sudo chown -R $ubuntu:$ubuntu /var/www/$TESTUSER.devopslab.xyz/public_html
+sudo chmod -R 755 /var/www
+
+# Copies default configuration files to create new ones for these sites
+
+sudo cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/$USERNAME.devopslab.xyz.conf
+sudo cp /etc/apache2/sites-available/$USERNAME.devopslab.xyz.conf /etc/apache2/sites-available/$TESTUSER.devopslab.xyz.conf
+
+
---
+++ scripts/functions-apache.sh
@@ -0,0 +1,24 @@
+
+function header
+{
+		echo ""
+		echo ""
+		echo "=============== $1 ==============="
+		echo ""
+}
+
+function
+{
+	if which apache2 > /dev/null; then
+		echo "Apache2 is already installed"
+	else
+		echo "Installing apache2"
+		sudo apt-get install apache2
+	fi
+
+	apache2 -v
+	echo "==================================="
+	echo ""
+}
+
+
